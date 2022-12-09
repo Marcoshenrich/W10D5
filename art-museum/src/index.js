55048bb51a7e4c419fd7673bcb660c713c51e0ed
Marcos <jmarcoshenrich@gmail.com>
2022-12-09 15:03:02 -0800

    api setup

---
+++ art-museum/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Root from './Root';
+// import {BrowserRouter} from 'react-router-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
